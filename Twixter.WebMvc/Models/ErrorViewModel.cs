7ff54cca769100ced28d32645e9f515de10854de
RumpleSteelSkin <osmnistbayrak@gmail.com>
2025-02-21 16:11:32 +0300

    N-tier base

---
+++ Twixter.WebMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Twixter.WebMvc.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
